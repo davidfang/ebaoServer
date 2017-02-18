aac32aeb9d72ab36b1fcb1896a9db9cd0ff61f24
fdjiangwu <fdjiangwu@yeah.net>
2017-02-18 23:58:09 +0800

    调整格式

--- schema/GoodSchema.js
+++ schema/GoodSchema.js
@@ -5,13 +5,11 @@ module.exports = new mongoose.Schema({
     desc: String,
     url: String,
     price: String,
-
     //关联字段,发布者
     publisher: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-
     //关联字段,评论列表
     comments: [
         {
